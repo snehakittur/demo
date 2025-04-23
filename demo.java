b67db378706d8e900a401be6198bbb8d68334932
snehakittur <snehakittur17@gmail.com>
2025-04-23 15:48:37 +0530

    Add and Sub

---
+++ demo.java
@@ -0,0 +1,9 @@
+class demo{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum = a+b;
+        int sub = a-b;
+        System.out.println("Additon is "+sum+ " and Substraction is "+sub);
+    }
+}
