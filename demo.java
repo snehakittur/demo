a4e31a92981e61ca7617473da0cd026cc0e82ce8
snehakittur <snehakittur17@gmail.com>
2025-04-23 15:57:59 +0530

    Mul and Div

--- demo.java
+++ demo.java
@@ -4,6 +4,9 @@ class demo{
         int b=5;
         int sum = a+b;
         int sub = a-b;
+        int mul =a*b;
+        int div = a/b;
         System.out.println("Additon is "+sum+ " and Substraction is "+sub);
+        System.out.println("Mutiplication is "+mul+ " and division is "+div);
     }
 }
